6e12e16151daecbaa2dcac29ee5a77a33767251d
Zack <zholloway92@gmail.com>
2017-03-31 16:50:05 -0400

    first commit

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "1";
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "2";
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "3";
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "4";
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "5";
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "6";
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "7";
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "8";
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "9";
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "0";
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "+";
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "-";
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "*";
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += "/";
+        }
+
+        private void Decimal_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWindow.Text += ".";
+        }
+
+        /*
+        private void Plus_Minus_Click(object sender, RoutedEventArgs e)
+        {
+            var textWindow = NumberWindow.Text;
+            textWindow.Trim(' ');
+
+            if (textWindow.Contains('+'))
+            {
+                var splitWindow = textWindow.Split('+');
+                var number = ((Double.Parse(splitWindow[1])) * -1).ToString();
+                NumberWindow.Text = $"{splitWindow[0]}+{number}";
+            }
+            if (textWindow.Contains('-'))
+            {
+                
+            }
+
+            if (textWindow.Contains('*'))
+            {
+               
+            }
+
+            if (textWindow.Contains('/'))
+            {
+                
+            }
+        }
+        */
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            var textWindow = NumberWindow.Text;
+            textWindow.Trim(' ');
+
+            if (textWindow.Contains('+'))
+            {
+                NumberWindow.Text = Math.Addition('+', textWindow);
+            }
+
+            if (textWindow.Contains('-'))
+            {
+                NumberWindow.Text = Math.Subtraction('-', textWindow);
+            }
+
+            if (textWindow.Contains('*'))
+            {
+                NumberWindow.Text = Math.Multiplication('*', textWindow);
+            }
+
+            if (textWindow.Contains('/')) //10417
+            {
+                NumberWindow.Text = Math.Division('/', textWindow);
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            var charList = new List<char>();
+
+            foreach (var character in NumberWindow.Text)
+            {
+                charList.Add(character);
+            }
+
+            charList.RemoveAt(charList.Count - 1);
+            NumberWindow.Text = String.Empty;
+
+            foreach (var character in charList)
+            {
+                NumberWindow.Text += character;
+            }
+        }
+    }
+}
---
+++ Calculator/Math.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Math
+    {
+        public static string Addition (char sign, string textWindow)
+        {
+            var splitArray = textWindow.Split(sign);
+            splitArray[0].Trim(' ');
+            var number1 = Double.Parse(splitArray[0]);
+            splitArray[1].Trim(' ');
+            var number2 = Double.Parse(splitArray[1]);
+            var sum = number1 + number2;
+            return sum.ToString();
+        }
+
+        public static string Subtraction(char sign, string textWindow)
+        {
+            var splitArray = textWindow.Split(sign);
+            splitArray[0].Trim(' ');
+            var number1 = Double.Parse(splitArray[0]);
+            splitArray[1].Trim(' ');
+            var number2 = Double.Parse(splitArray[1]);
+            var difference = number1 - number2;
+            return difference.ToString();
+        }
+
+        public static string Multiplication(char sign, string textWindow)
+        {
+            var splitArray = textWindow.Split(sign);
+            splitArray[0].Trim(' ');
+            var number1 = Double.Parse(splitArray[0]);
+            splitArray[1].Trim(' ');
+            var number2 = Double.Parse(splitArray[1]);
+            var product = number1 * number2;
+            return product.ToString();
+        }
+
+        public static string Division(char sign, string textWindow)
+        {
+            var splitArray = textWindow.Split(sign);
+            splitArray[0].Trim(' ');
+            var number1 = Double.Parse(splitArray[0]);
+            splitArray[1].Trim(' ');
+            var number2 = Double.Parse(splitArray[1]);
+            var quotient = number1 / number2;
+            return quotient.ToString();
+        }
+    }
+}
